f7f82aec4744d467f36aa5184942eba24b09730e
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 10:54:44 -0300

    installing and importing email-validator from pydantic lib

--- contract.py
+++ contract.py
@@ -1,10 +1,10 @@
 from typing import Tuple
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Sales(BaseModel):
-    email: str
+    email: EmailStr
     date_time: datetime
     product_value: float
     product_quantity: int
