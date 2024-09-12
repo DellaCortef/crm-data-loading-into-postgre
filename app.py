70a84c840e86abe617d1e7c8c6d58f59a02332d5
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 10:15:14 -0300

    FIX: fixing time variable to purchase_time

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def main():
 
     # Input fields for data
     email = st.text_input("Email do Vendedor")
-    data = st.date_input("Data da compra", datetime.now())
-    time = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
+    purchase_data = st.date_input("Data da compra", datetime.now())
+    purchase_time = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
     product_value = st.number_input("Valor da venda", min_value=0.0, format="%.2f")
     quantity = st.number_input("Quantidade de produtos", min_value=1, step=1)
     product_type = st.selectbox("Produto", options=["ZapFlow com Gemini", "ZapFlow com chatGPT", "ZapFlow com Llama 3.0"])
@@ -16,7 +16,7 @@ def main():
     # Submit button
     if st.button("Salvar"):
         # Combining the selected date and time to create the datetime
-        date_time = datetime.combine(data, time)
+        date_time = datetime.combine(purchase_data, purchase_time)
 
         # Displaying the data on the screen
         st.write("**Dados da Venda:**")
