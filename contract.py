1dd92c48fac480c73479f77007863a5016d18c38
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 11:06:35 -0300

    importing PositiveFloat and PositiveInt to set into vals

--- contract.py
+++ contract.py
@@ -1,14 +1,14 @@
 from enum import Enum
 from typing import Tuple
 from datetime import datetime
-from pydantic import BaseModel, EmailStr, validate_call
+from pydantic import BaseModel, EmailStr, validate_call, PositiveFloat, PositiveInt
 
 
 class Sales(BaseModel):
     email: EmailStr
     date_time: datetime
-    product_value: float
-    product_quantity: int
+    product_value: PositiveFloat
+    product_quantity: PositiveInt
     product_type: ProductEnum
 
 class ProductEnum(str, Enum):
