2de9df4407b456024c0ebb7df2ab176e7d75a675
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 10:42:12 -0300

    changing quantity val to product_quantity name

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     purchase_data = st.date_input("Data da compra", datetime.now())
     purchase_time = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
     product_value = st.number_input("Valor da venda", min_value=0.0, format="%.2f")
-    quantity = st.number_input("Quantidade de produtos", min_value=1, step=1)
+    product_quantity = st.number_input("Quantidade de produtos", min_value=1, step=1)
     product_type = st.selectbox("Produto", options=["ZapFlow com Gemini", "ZapFlow com chatGPT", "ZapFlow com Llama 3.0"])
 
     # Submit button
@@ -24,7 +24,7 @@ def main():
         st.write(f"Email do Vendedor: {seller_email}")
         st.write(f"Data e Hora da Compra: {date_time}")
         st.write(f"Valor da Venda: R$ {product_value:.2f}")
-        st.write(f"Quantidade de Produtos: {quantity}")
+        st.write(f"Quantidade de Produtos: {product_quantity}")
         st.write(f"Produto: {product_type}")
 
 if __name__ == "__main__":
