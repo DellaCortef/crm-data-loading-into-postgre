35c9442a9951710dd101940c8e42878f9fa7c77b
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 11:53:57 -0300

    FIX: fixing contract.py and app.py files to run the application

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from contract import Sales
 from datetime import datetime, time
 from pydantic import ValidationError
+from contract import Sales, ProductEnum
 
 # Main function for the frontend
 def main():
-    st.title("Sistema de CRM e Vendas da ZapFlow")
+    st.title("ZapFlow CRM and Sales System")
 
     # Input fields for data
     seller_email = st.text_input("Email do Vendedor")
@@ -13,7 +13,7 @@ def main():
     purchase_time = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
     product_value = st.number_input("Valor da venda", min_value=0.0, format="%.2f")
     product_quantity = st.number_input("Quantidade de produtos", min_value=1, step=1)
-    product_type = st.selectbox("Produto", options=["ZapFlow com Gemini", "ZapFlow com chatGPT", "ZapFlow com Llama 3.0"])
+    product_type = st.selectbox("Produto", options=[e.value for e in ProductEnum])
 
     # Submit button
     if st.button("Salvar"):
@@ -23,9 +23,7 @@ def main():
             # Combining the selected date and time to create the datetime
             date_time = datetime.combine(purchase_data, purchase_time)
 
-            # importing contract classes
-            contract(seller_email, purchase_data, purchase_time, product_value, product_quantity, product_type)
-
+            # Validating data with Pydantic
             sale = Sales(
                 email = seller_email,
                 date_time = date_time,
@@ -38,8 +36,9 @@ def main():
 
         except ValidationError as e:
             st.error("Something went wrong: {e}")
-
-
+        
+        except Exception as e:
+            st.error(f"Error saving data: {e}")
 
 if __name__ == "__main__":
     main()
