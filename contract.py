e1ceae90e5cd6925f034d68afbb9edb79d0e49ed
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 10:43:29 -0300

    creating our classes types

--- contract.py
+++ contract.py
@@ -3,3 +3,9 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
+class Sales(BaseModel):
+    email: str
+    date_time: datetime
+    product_value: float
+    product_quantity: int
+    product_type: str
