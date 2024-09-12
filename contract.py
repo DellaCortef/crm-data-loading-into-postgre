98f8f23bc1e7e51fb9d6aeedf2c1f9461cc7b691
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 11:01:24 -0300

    importing Enum from enum lib

--- contract.py
+++ contract.py
@@ -1,6 +1,7 @@
+from enum import Enum
 from typing import Tuple
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validate_call
 
 
 class Sales(BaseModel):
@@ -9,3 +10,5 @@ class Sales(BaseModel):
     product_value: float
     product_quantity: int
     product_type: str
+
+class ProductEnum(str, enum):
