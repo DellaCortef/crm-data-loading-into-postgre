809945b55152cb9eceaf655728699429243bcfd8
DellaCortef <felipe.dellacorte@outlook.com.br>
2024-09-12 21:03:58 -0300

    creating .env file to our env vals

---
+++ database.py
@@ -0,0 +1,26 @@
+import os
+import psycopg2
+from psycopg2 import sql
+from contract import Sales
+from dotenv import load_dotenv
+
+from sqlalchemy import select, bindparam
+scalar_subq = (
+     select(user_table.c.id)
+     .where(user_table.c.name == bindparam("username"))
+     .scalar_subquery()
+)
+
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(address_table).values(user_id=scalar_subq),
+        [
+             {
+                 "username": "spongebob",
+                 "email_address": "spongebob@sqlalchemy.org",
+             },
+             {"username": "sandy", "email_address": "sandy@sqlalchemy.org"},
+             {"username": "sandy", "email_address": "sandy@squirrelpower.org"},
+        ],
+    )
+    conn.commit()
